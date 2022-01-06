4957a32b18bb01239894ddc4b30f120152ec23f2
Ian Clark <ianclark226@gmail.com>
2022-01-06 14:25:46 -0500

    properties and enquiries

---
+++ backend/apps/enquiries/serializers.py
@@ -0,0 +1,8 @@
+from django.db.models import fields
+from rest_framework import serializers
+from .models import Enquiry
+
+class EnquirySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Enquiry
+        fields = "__all__"
