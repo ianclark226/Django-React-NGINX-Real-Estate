ddc0c849f0ac3f93795f2611c3b7734547d40854
Ian Clark <ianclark226@gmail.com>
2022-05-16 15:14:35 -0400

    commit before revert

--- backend/client/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import { propertiesListReducer } from "./propertyReducer";
-
-export default combineReducers({
-	propertiesList: propertiesListReducer,
-});
